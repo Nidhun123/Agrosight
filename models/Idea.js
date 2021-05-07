06df8e33e6950fab9b76bd7be0f3685b197f099a
Thomas Alphonse Benny <48207504+alphonse6171@users.noreply.github.com>
2021-05-07 21:20:41 +0530

    Add files via upload

---
+++ models/Idea.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+const IdeaSchema = new Schema({
+    // category:{
+    //     type: Number,
+    //     required: true
+    // },
+    name:
+    {
+        type: String,
+        required: true
+    },
+    price:
+    {
+        type: Number,
+        required: true
+    },
+    exp_date:{
+        type: String,
+        default: Date.now
+    },
+    pkd_date:{
+        type: String,
+        default: Date.now
+    },
+    quantity:{
+        type: String,
+        required: true
+    },
+    discount:{
+        type: Number,
+        required: true
+    }
+});
+
+mongoose.model('ideas', IdeaSchema);
