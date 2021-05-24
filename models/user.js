38fa1c02c4458938a4d0bf8468abaaac7513fdf4
Thomas Alphonse Benny <48207504+alphonse6171@users.noreply.github.com>
2021-05-24 10:06:25 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create schema
+const UserSchema = new Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    date_of_birth: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+mongoose.model('users', UserSchema);
